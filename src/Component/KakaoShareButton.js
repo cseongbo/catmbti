997e3b8e55a96ce339d115f7e321833be0b53a22
seongbochoi <cseongbo17@gmail.com>
2022-08-30 19:24:54 +0900

    kaka2

--- src/Component/KakaoShareButton.js
+++ src/Component/KakaoShareButton.js
@@ -2,23 +2,23 @@ import React from "react";
 import Button from 'react-bootstrap/Button'
 const {Kakao} = window;
 
-const KakaoShareButton = ({data}) =>{
+const KakaoShareButton = () =>{
     const url = "https://catmbti0829.netlify.app"
     const resultUrl = window.location.href;
-    console.log('data',data)
     console.log('aaa',resultUrl,url)
     React.useEffect(()=>{
         Kakao.cleanup()
         Kakao.init("9f353f4b2155ea47128863355b687761")
         console.log(Kakao.isInitialized)
     },[])
+
     const shareKakao = () =>{
       Kakao.Share.sendDefault({
           objectType: 'feed',
           content: {
             title: '예비집사 판별기 결과',
-            description: `예비 집사님이 고양이를 키운다면 가장 잘 맞는 고양이는 ${data.name} 입니다.`,
-            imageUrl: url + data.image,
+            description: `예비 집사님이 고양이를 키운다면 가장 잘 맞는 고양이는?`,
+            imageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
             link: {
               mobileWebUrl: resultUrl,
               webUrl : resultUrl
